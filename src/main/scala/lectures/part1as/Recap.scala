b74f06d20d4cb596ad01a262ccc82e01b75a30c3
Manish Karki <mannishkarki@gmail.com>
2020-08-26 21:53:27 -0400

    code block

--- src/main/scala/lectures/part1as/Recap.scala
+++ src/main/scala/lectures/part1as/Recap.scala
@@ -4,9 +4,10 @@ package lectures.part1as
  * @author mkarki
  */
 object Recap extends App {
+  val condition = false
   val aCodeBlock = {
-    if(true) 54
-    "manish"
+    if(true) 54 // value of codeBlock is last line so this block is almost unused
+    72
   }
   print(aCodeBlock)
 }

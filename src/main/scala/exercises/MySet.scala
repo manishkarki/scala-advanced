b01b4173e54f3ed76f1b5f1fb224d3b43b99810a
Manish Karki <mannishkarki@gmail.com>
2020-09-01 22:43:00 -0400

    testing functionality

--- src/main/scala/exercises/MySet.scala
+++ src/main/scala/exercises/MySet.scala
@@ -98,5 +98,5 @@ object MySet {
 
 object MySetPlayground extends App {
   val s = MySet(1,2,3,4)
-  s + 5 foreach println
+  s + 5 ++ MySet(-1, -2) + 3 flatMap ( x => MySet(x, 10 *x)) filter(_ % 2 == 0) foreach println
 }

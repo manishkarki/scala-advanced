de396c0b43ceb4683935da71e6802425cc66a114
Manish Karki <mannishkarki@gmail.com>
2020-09-01 21:37:09 -0400

    debugging with print

--- src/main/scala/lectures/part1as/AdvancedPatternMatching.scala
+++ src/main/scala/lectures/part1as/AdvancedPatternMatching.scala
@@ -80,4 +80,12 @@ object AdvancedPatternMatching extends App {
     }
   }
 
+  val myList: MyList[Int] = Cons(1, Cons(2, Cons(3, Empty)))
+  val decomposed = myList match {
+    case MyList(1, 2, _*) => "starting with 1, 2"
+    case _=> "not starting with 1, 2"
+  }
+
+  print(decomposed)
+
 }
